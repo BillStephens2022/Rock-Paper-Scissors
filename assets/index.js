1615f7ae8fa8f82d337a0eec5228f8fed6081e4c
BillStephens2022 <two4onebill@yahoo.com>
2022-12-29 16:53:27 -0500

    reorganized code and added comments and some minor styling changes

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,9 @@
+
+// HTML element variables
 const buttonDivEl = $('.button-div');
 const yourPickEl = $('#your-pick');
 const computerPickEl = $('#computer-pick');
 const winLossMessage = $('#win-loss-message');
-const rpsArray = ['rock', 'paper', 'scissors'];
 const rockButton = $('#rock');
 const paperButton = $('#paper');
 const scissorsButton = $('#scissors');
@@ -12,6 +13,9 @@ const winsEl = $('#wins');
 const lossesEl = $('#losses');
 const tiesEl = $('#ties');
 const resetScoresButton = $('#reset-scores')
+
+// other variables
+const rpsArray = ['rock', 'paper', 'scissors'];
 let yourPick;
 let computerPick;
 let computerPickImg;
@@ -19,12 +23,26 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 
+//game initilization
+init();
 
+/* initialization - enables buttons so player can make choice, clears choices from previous game,
+and empties win/loss messages.  Scoreboard is also updated based on values in local storage */
+function init() {
+    enableButtons();
+    winLossMessage.empty();
+    computerPickEl.empty();
+    yourPickEl.empty();
+    hidePlayAgainButton();
+    getWins();
+    getLosses();
+    getTies();
+}
 
-$(buttonDivEl).click(playerSelection);
-$(playAgainButton).click(init);
-$(resetScoresButton).click(resetScores);
+/*-------------------Gameplay Functions----------------*/
 
+/* function for player selection - once player makes a choice, choice will be rendered on page as an image,
+buttons will be disabled, computer selection will be made, and results checked. */
 function playerSelection(event) {
     let rps = event.target;
     if (rps.id === "rock") {
@@ -47,12 +65,15 @@ function playerSelection(event) {
     };
 }
 
+// function for random computer choice.  Computer choice will be rendered on the page as an image.
 function computerSelection() {
     computerPick = rpsArray[Math.floor(Math.random() * rpsArray.length)];
     computerPickImg = `./assets/images/${computerPick}.png`;
     computerPickEl.prepend(`<img src="${computerPickImg}" alt="${computerPick}" id="computer-pick-img">`);
 }
 
+/* function to check results to see whether player or computer won, or if it was a tie. Scores are
+incremented and saved to local storage*/
 function checkResult(yourPick) {
     if (yourPick === computerPick) {
         winLossMessage.text("It's a TIE!!");
@@ -91,43 +112,36 @@ function checkResult(yourPick) {
         wins++;
         setWins();
     }
-    showPlayAgainButton();
-    
+    showPlayAgainButton();  //player is prompted to play again with a button
 }
 
-init();
-
+// disables user choice buttons after selection is made.
 function disableButtons() {
     rockButton.prop('disabled', true);
     paperButton.prop('disabled', true);
     scissorsButton.prop('disabled', true);
 }
 
+// enables user choice buttons once init() is called. When player wants to play again, buttons will be enabled.
 function enableButtons() {
     rockButton.prop('disabled', false);
     paperButton.prop('disabled', false);
     scissorsButton.prop('disabled', false);
 }
 
+// user is prompted with a button to play again.
 function showPlayAgainButton() {
     playAgainButton.css('visibility', 'visible');
 }
 
+// play again button is hidden when game is started/re-started.
 function hidePlayAgainButton() {
     playAgainButton.css('visibility', 'hidden');
 }
 
-function init() {
-    enableButtons();
-    winLossMessage.empty();
-    computerPickEl.empty();
-    yourPickEl.empty();
-    hidePlayAgainButton();
-    getWins();
-    getLosses();
-    getTies();
-}
+/*-------------------Scoreboard Functions----------------*/
 
+// function to get Wins from Local Storage
 function getWins() {
     var storedWins = localStorage.getItem("wins");
     if (storedWins === null) {
@@ -138,6 +152,7 @@ function getWins() {
     winsEl.text(wins);
 }
 
+// function to get Losses from Local Storage
 function getLosses() {
     var storedLosses = localStorage.getItem("losses");
     if (storedLosses === null) {
@@ -148,6 +163,7 @@ function getLosses() {
     lossesEl.text(losses);
 }
 
+// function to get Ties from Local Storage
 function getTies() {
     var storedTies = localStorage.getItem("ties");
     if (storedTies === null) {
@@ -158,21 +174,25 @@ function getTies() {
     tiesEl.text(ties);
 }
 
+// function to save wins to Local Storage
 function setWins() {
     winsEl.textContent = wins;
     localStorage.setItem("wins", wins);
 }
 
+// function to save losses to Local Storage
 function setLosses() {
     lossesEl.textContent = losses;
     localStorage.setItem("losses", losses);
 }
 
+// function to save ties to Local Storage
 function setTies() {
     tiesEl.textContent = ties;
     localStorage.setItem("ties", ties);
 }
 
+// resets wins, losses, and ties to zero in Local Storage
 function resetScores() {
     wins = 0;
     losses = 0;
@@ -182,5 +202,7 @@ function resetScores() {
     setTies();
 }
 
-
-
+// Event listeners
+$(buttonDivEl).click(playerSelection);
+$(playAgainButton).click(init);
+$(resetScoresButton).click(resetScores);
