df654c3dfd1a6db07471f3f66f193a13f70d25b6
BillStephens2022 <two4onebill@yahoo.com>
2022-12-29 15:56:18 -0500

    added score tracking in local storage

--- assets/index.js
+++ assets/index.js
@@ -8,6 +8,10 @@ const paperButton = $('#paper');
 const scissorsButton = $('#scissors');
 const endGame = $('.end-game');
 const playAgainButton = $('#play-again');
+const winsEl = $('#wins');
+const lossesEl = $('#losses');
+const tiesEl = $('#ties');
+const resetScoresButton = $('#reset-scores')
 let yourPick;
 let computerPick;
 let computerPickImg;
@@ -18,6 +22,8 @@ let ties = 0;
 
 
 $(buttonDivEl).click(playerSelection);
+$(playAgainButton).click(init);
+$(resetScoresButton).click(resetScores);
 
 function playerSelection(event) {
     let rps = event.target;
@@ -51,35 +57,45 @@ function checkResult(yourPick) {
     if (yourPick === computerPick) {
         winLossMessage.text("It's a TIE!!");
         winLossMessage.css('color', 'yellow');
+        ties++;
+        setTies();
     } 
     else if (yourPick === 'rock' && computerPick === "scissors") {
         winLossMessage.text("Rock breaks scissors!  You WIN!!");
         winLossMessage.css('color', 'green');
         wins++;
+        setWins();
     } else if (yourPick === 'rock' && computerPick === "paper") {
         winLossMessage.text("Paper covers Rock! You LOSE!!");
         winLossMessage.css('color', 'red');
         losses++;
+        setLosses();
     } else if (yourPick === 'paper' && computerPick === "rock") {
         winLossMessage.text("Paper covers Rock! You WIN!!");
         winLossMessage.css('color', 'green');
         wins++
+        setWins();
     } else if (yourPick === 'paper' && computerPick === 'scissors') {
         winLossMessage.text("Scissors cuts Paper!  You LOSE!!");
         winLossMessage.css('color', 'red');
         losses++;
+        setLosses();
     } else if (yourPick === 'scissors' && computerPick === 'rock') {
         winLossMessage.text("Rock breaks Scissors! You LOSE!!");
         winLossMessage.css('color', 'red');
         losses++;
+        setLosses();
     } else if (yourPick === 'scissors' && computerPick === 'paper') {
         winLossMessage.text("Scissors cuts Paper! You WIN!!");
         winLossMessage.css('color', 'green');
         wins++;
+        setWins();
     }
     showPlayAgainButton();
     
 }
+
+init();
 
 function disableButtons() {
     rockButton.prop('disabled', true);
@@ -101,20 +117,70 @@ function hidePlayAgainButton() {
     playAgainButton.css('visibility', 'hidden');
 }
 
-$(playAgainButton).click(init);
-
-
 function init() {
     enableButtons();
     winLossMessage.empty();
     computerPickEl.empty();
     yourPickEl.empty();
     hidePlayAgainButton();
+    getWins();
+    getLosses();
+    getTies();
 }
 
+function getWins() {
+    var storedWins = localStorage.getItem("wins");
+    if (storedWins === null) {
+        wins = 0;
+    } else {
+        wins = storedWins;
+    };
+    winsEl.text(wins);
+}
 
+function getLosses() {
+    var storedLosses = localStorage.getItem("losses");
+    if (storedLosses === null) {
+        losses = 0;
+    } else {
+        losses = storedLosses;
+    };
+    lossesEl.text(losses);
+}
 
+function getTies() {
+    var storedTies = localStorage.getItem("ties");
+    if (storedTies === null) {
+        ties = 0;
+    } else {
+        ties = storedTies;
+    };
+    tiesEl.text(ties);
+}
 
+function setWins() {
+    winsEl.textContent = wins;
+    localStorage.setItem("wins", wins);
+}
+
+function setLosses() {
+    lossesEl.textContent = losses;
+    localStorage.setItem("losses", losses);
+}
+
+function setTies() {
+    tiesEl.textContent = ties;
+    localStorage.setItem("ties", ties);
+}
+
+function resetScores() {
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    setWins();
+    setLosses();
+    setTies();
+}
 
 
 
